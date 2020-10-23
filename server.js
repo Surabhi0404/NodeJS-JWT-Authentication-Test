484bd7f385f64c4b472f52e93d2c69ddf12f973c
Surabhi0404 <surabhigurav96@gmail.com>
2020-10-22 23:47:05 -0400

    Added settings route

--- server.js
+++ server.js
@@ -70,6 +70,12 @@ app.get('/api/dashboard', jwtMW, (req, res) => {
     });
 });
 
+app.get('/api/settings', jwtMW, (req, res) => {
+    res.json({
+        success: true,
+        myContent: 'This is the settings page'
+    });
+});
 
 app.get('/api/prices', jwtMW, (req, res) => {
     res.json({
